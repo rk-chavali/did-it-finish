07c3afe939ac3d835d412cd5fbf14886abc11839
Rama Krishna Chavali <chavali.r@northeastern.edu>
2026-09-23 00:31:38 -0400

    test: add demo change with deliberate unfinished work

---
+++ demo/invoice.test.ts
@@ -0,0 +1,5 @@
+import { describe, it } from "vitest";
+
+describe.skip("parseInvoice", () => {
+  it("parses totals", () => {});
+});
---
+++ demo/invoice.ts
@@ -0,0 +1,4 @@
+export function parseInvoice(text: string): number {
+  // TODO: support multi-currency invoices
+  throw new Error("Not implemented");
+}
