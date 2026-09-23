2e9f464079da431218e3b335644a07085a4c7556
Rama Krishna Chavali <chavali.r@northeastern.edu>
2026-09-23 00:30:29 -0400

    chore: scaffold TypeScript action with eslint, vitest, and ncc

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["dist/", "node_modules/", "coverage/"] },
+  js.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: { projectService: true, tsconfigRootDir: import.meta.dirname },
+    },
+  },
+  { files: ["eslint.config.js"], ...tseslint.configs.disableTypeChecked },
+);
